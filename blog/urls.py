f43ae3b7f85d1ed70803334390cd1b56df9c3d24
zakharkevich94v <zakharkevich1994@yandex.by>
2021-12-19 15:20:25 +0300

    add test logic

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
-    
+    path('', views.home, name='home'), 
+    path('all_vacancy', views.all_vacancy, name='all_vacancy'),  
+    path('one_vacancy/<vacncy_id>', views.description_one_vacancy, name='one_vacancy'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from blog.models import GeneralPageInformation, Vacancy 
 
-# Create your views here.
+
+def home(request):
+    company_information = GeneralPageInformation.objects.all()
+    context = {'company_information': company_information}
+    return render(request, 'blog/home_page.html', context)
+
+
+def all_vacancy(request):
+    vacancy = Vacancy.objects.all()
+    context = {'vacancy': vacancy}
+    return render(request, 'blog/all_vacancy.html', context)
+
+
+def description_one_vacancy(request, vacancy_id):
+    one_vacancy = Vacancy.objects.get(pk=vacancy_id)
+    context = {'one_vacancy': one_vacancy}
+    return render(request, 'blog/about_one_vacancy.html', context)
